1fe77694fb8b2e7904813413526ae5bcbd8c475a
Pablo Hernández <devphernandez@gmail.com>
2015-12-15 00:06:16 -0300

    conexion pdo
    mejorada

--- application/class/database.php
+++ application/class/database.php
@@ -3,10 +3,14 @@
 	class Database extends PDO{
 
 		public function __construct(){
+			
+			try{
+				parent::__construct('mysql:host=localhost;dbname=optic a_db','root','');
+				parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}catch(PDOException $e){
+				die('No se puede establecer la conexión al servidor.');
+			}
 
-			parent::__construct('mysql:host=localhost;dbname=optica_db','root','');
-			parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
 		}
 
 	}
