acc94977201f8d58e1da0c53bbfec8215e86e54c
apeeclgithub <apeeclgithub@gmail.com>
2016-01-28 23:03:45 -0300

    venta

--- application/views/venta.php
+++ application/views/venta.php
@@ -65,6 +65,23 @@
 	</div>
 	<div id="tablaProducts">
 		<?php require '../controller/selectProductSailDetail.php'; ?>
+	<div id="dataClient">
+		<div class="mdl-textfield mdl-js-textfield">
+			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleTotal">
+			<label for="amountSaleTotal">Total</label>
+			<span class="mdl-textfield__error">Ingresar solo números</span>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield">
+			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleTotalIva">
+			<label for="amountSaleTotalIva">Total con Iva</label>
+			<span class="mdl-textfield__error">Ingresar solo números</span>
+		</div>
+		<div class="mdl-textfield mdl-js-textfield">
+			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleTotalFinal">
+			<label for="amountSaleTotalFinal">Total</label>
+			<span class="mdl-textfield__error">Ingresar solo números</span>
+		</div>
+	</div>
 	</div>
 	<div id="dataClient">
 		<div class="fontItem">MEDIO DE PAGO</div>
@@ -107,19 +124,27 @@
 		</div>
 	</div>
 	<div id="dataClient">
+		<div class="fontItem">CHEQUE</div>
 		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleCheque">
-			<label class="mdl-textfield__label" for="amountSaleCheque">Cheque</label>
+			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="checkSailNumber">
+			<label class="mdl-textfield__label" for="checkSailNumber">Número Cheque</label>
 			<span class="mdl-textfield__error">Ingresar solo números</span>
 		</div>
 		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleCheque">
-			<label class="mdl-textfield__label" for="amountSaleCheque">Cheque</label>
-			<span class="mdl-textfield__error">Ingresar solo números</span>
+			<input class="mdl-textfield__input" type="text" id="checkSailBank">
+			<label class="mdl-textfield__label" for="checkSailBank">Banco</label>
+		</div>
+		<br><br>Fecha Cheque<br>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input id="checkSailDate" class="mdl-textfield__input" type="date">	
+		</div>
+		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+			<input class="mdl-textfield__input" type="text" id="checkSailTitular">
+			<label class="mdl-textfield__label" for="checkSailTitular">Titular</label>
 		</div>
 		<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
 			<input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleCheque">
-			<label class="mdl-textfield__label" for="amountSaleCheque">Cheque</label>
+			<label class="mdl-textfield__label" for="amountSaleCheque">Monto</label>
 			<span class="mdl-textfield__error">Ingresar solo números</span>
 		</div>
 	</div>
