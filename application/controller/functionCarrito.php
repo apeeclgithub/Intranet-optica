7b3e95298aa94c65c60531737216a7616ab79f52
Pablo Hernández <devphernandez@gmail.com>
2016-02-05 13:24:11 -0300

    correccion iva

--- application/controller/functionCarrito.php
+++ application/controller/functionCarrito.php
@@ -40,11 +40,20 @@ switch($page){
     
     case 3:
     ?>
-    
-    <input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleTotal" 
-    value="<?php echo $carrito->precio_total();?>">
-    <label for="amountSaleTotal">Total</label>
-    <span class="mdl-textfield__error">Ingresar solo números</span>
+    <div id="dataClient">
+        <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="[0-9]*" id="amountSaleTotal" 
+            value="<?php echo $carrito->precio_total();?>">
+            <label for="amountSaleTotal">Total</label>
+            <span class="mdl-textfield__error">Ingresar solo números</span>
+        </div>
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="checkbox-iva">
+                <input onclick="updateTotalIva()" type="checkbox" id="checkbox-iva" class="mdl-checkbox__input">
+                <span class="mdl-checkbox__label">CON IVA</span>
+            </label>    
+        </div>
+    </div>
     
     <?php
     break;
