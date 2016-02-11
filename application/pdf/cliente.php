49664c953416979f0b72b878a830542f40ebfc88
apeeclgithub <apeeclgithub@gmail.com>
2016-02-11 00:04:10 -0300

    pdf clientes

--- application/pdf/cliente.php
+++ application/pdf/cliente.php
@@ -10,7 +10,7 @@ class PDF extends FPDF
 	function Header()
 	{
 
-		$this->Image('../../public/img/logo.png',87,8,33);
+		$this->Image('../../public/img/logo.png',130,8,33);
 
 		$this->SetFont('Arial','',12);
 		$this->Ln(10);
@@ -22,49 +22,48 @@ class PDF extends FPDF
 		$this->Cell(0,10, 'Mac Iver 180, Oficina 35',0,0,'C');
 		$this->Ln(5);
 		$this->Cell(0,10, 'Importadoralypltda@gmail.com',0,0,'C');
-		$this->Ln(10);
-		$this->Cell(40,5,"N. de Venta: ");
-		$this->Ln(5);
-		$this->Cell(40,5,"Fecha: ");
 	}
 
 //Pie de página
-  function Footer()
-  {
-
-  	$this->SetY(260);
-  	$this->Cell(40,5, 'Fecha Entrega: ');
-  	$this->Cell(210,5, utf8_decode('Total:'),0,0,'C');
-  	$this->Ln(10);
-  	$this->Cell(40,5,'Firma Cliente: ');
-  	$this->Cell(212,5, utf8_decode('Abono:'),0,0,'C');
-  	$this->Ln(10);
-  	$this->Cell(40,5, 'Firma Vendedor: ');
-  	$this->Cell(210,5, utf8_decode('Saldo:'),0,0,'C');
-
-  }
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Page number
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
 
 }
 //Creación del objeto de la clase heredada
-$pdf=new FPDF('L','mm','A4');
+$pdf=new PDF('L','mm','A4');
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
 $pdf->Ln(10);
 $pdf->Ln(10);
-
+$pdf->Cell(50,10, utf8_decode('Nombre'),1,0,'C');
+$pdf->Cell(30,10, utf8_decode('Rut'),1,0,'C');
+$pdf->Cell(25,10, utf8_decode('Fono'),1,0,'C');
+$pdf->Cell(25,10, utf8_decode('Celular'),1,0,'C');
+$pdf->Cell(80,10, utf8_decode('Dirección'),1,0,'C');
+$pdf->Cell(35,10, utf8_decode('Comuna'),1,0,'C');
+$pdf->Cell(35,10, utf8_decode('Giro'),1,0,'C');
+$pdf->Ln();
   	require_once '../model/classCliente.php';
   	$objClient = new Cliente();
   	$objClient->selectClientAll();
 
   	foreach ((array) $objClient as $key) {
   		foreach ($key as $key2 => $value) {
-  			$pdf->Cell(30,10,$value['cli_nombre'],1,0);
-  			$pdf->Cell(30,10,$value['cli_rut'],1,0);
-  			$pdf->Cell(30,5,$value['cli_fono'],1,0);
-  			$pdf->Cell(30,5,$value['cli_celular'],1,0);
-			$pdf->Cell(70,5,$value['cli_direccion'],1,0);
-			$pdf->Cell(25,5,$value['cli_comuna'],1,0);
-			$pdf->Cell(25,5,$value['cli_giro'],1,0);
+  			$pdf->Cell(50,10,utf8_decode($value['cli_nombre']),1,0);
+  			$pdf->Cell(30,10,utf8_decode($value['cli_rut']),1,0);
+  			$pdf->Cell(25,10,utf8_decode($value['cli_fono']),1,0);
+  			$pdf->Cell(25,10,utf8_decode($value['cli_celular']),1,0);
+			$pdf->Cell(80,10,utf8_decode($value['cli_direccion']),1,0);
+			$pdf->Cell(35,10,utf8_decode($value['cli_comuna']),1,0);
+			$pdf->Cell(35,10,utf8_decode($value['cli_giro']),1,0);
+			$pdf->Ln(10);
 
   		}
   	}
--- application/views/cliente.php
+++ application/views/cliente.php
@@ -47,7 +47,7 @@
 			<?php require '../controller/selectClientAll.php'; ?>
 		</div>
 		<div>
-			<button class="mdl-button mdl-button--raised mdl-button--colored">Exportar Listado</button>
+			<button class="mdl-button mdl-button--raised mdl-button--colored"><a href="../pdf/cliente.php" target="_blank">Exportar Listado</a></button>
 		</div>
 	</div>
 	<!--modal editar cliente -->
