48632469d7a24607d1465d002129b1ef9f3549e6
Pablo Hernández <devphernandez@gmail.com>
2016-05-19 18:39:16 -0400

    la mierda de la cintia
    manso webeo la mierda

--- application/controller/anularVenta.php
+++ application/controller/anularVenta.php
@@ -1,19 +1,30 @@
 <?php
 	require_once '../model/classAnular.php';
-	//$venta = $_POST['ventaId'];
-	$venta = 1;
+	$venta = $_POST['ventaId'];
 	$objVenta = new Anular();
 	$objVenta->existeVenta($venta);
 	$json['success'] = false;
+	$isOK = false;
 				
 
 	foreach ((array) $objVenta as $key) {
 		foreach ($key as $value) {
 			if($value['ven_id'] == $venta){
+				$isOK = true;
 				$json['success'] = true;
 			}
 		}
     }
+
+    if($isOK == true){
+    	$objVenta->buscarProductos($venta);
+    	foreach ((array) $objVenta as $key) {
+			foreach ($key as $value) {
+				$objVenta->agregarProductos($value['producto_pro_id'], $value['det_cantidad']);
+			}
+		}
+    }
+	$objVenta->borrarVenta($venta);
 
 echo json_encode($json);
 
--- application/model/classAnular.php
+++ application/model/classAnular.php
@@ -14,7 +14,28 @@
 			$this->anular = $sql->fetchAll(PDO::FETCH_ASSOC);
 
 			return $this->anular;
+		}
 
+		public function buscarProductos($id){
+
+			$objConn = new Database();
+			$sql = $objConn->prepare('SELECT producto_pro_id, det_cantidad FROM detalle WHERE venta_ven_id = :id');
+			$sql->bindParam(':id', $id);
+			$sql->execute();
+			$this->anular = $sql->fetchAll(PDO::FETCH_ASSOC);
+			return $this->anular;
+		
+		}
+
+		public function agregarProductos($id, $cant){
+
+			$objConn = new Database();
+			$sql = $objConn->prepare('UPDATE producto SET pro_stock = pro_stock+:cant WHERE pro_id = :id');
+			$sql->bindParam(':id', $id);
+			$sql->bindParam(':cant', $cant);
+			$sql->execute();
+			return $this->anular;
+		
 		}
 
 		public function borrarVenta($id){
@@ -23,28 +44,23 @@
 			$sql = $objConn->prepare('DELETE FROM venta WHERE ven_id = :id');
 			$sql->bindParam(':id', $id);
 			$sql->execute();
-			$this->anular = $sql->fetchAll(PDO::FETCH_ASSOC);
-
+			$sql1 = $objConn->prepare('DELETE FROM tipo WHERE venta_ven_id = :id');
+			$sql1->bindParam(':id', $id);
+			$sql1->execute();
+			$sql2 = $objConn->prepare('DELETE FROM cheque WHERE tipo_tip_id = :id');
+			$sql2->bindParam(':id', $id);
+			$sql2->execute();
+			$sql3 = $objConn->prepare('DELETE FROM detalle WHERE venta_ven_id = :id');
+			$sql3->bindParam(':id', $id);
+			$sql3->execute();
 			return $this->anular;
-
+		
 		}
-
-		public function borrarDetalle($id){
-
-			$objConn = new Database();
-			$sql = $objConn->prepare('DELETE FROM venta WHERE ven_id = :id');
-			$sql->bindParam(':id', $id);
-			$sql->execute();
-			$this->anular = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-			return $this->anular;
-
-		}
-
+		
 	}
 
-	$objAnular = new Anular();
-	$objAnular->existeVenta(1);
-	var_dump($objAnular);
+	/*$objAnular = new Anular();
+	$objAnular->agregarProductos(2,5);
+	var_dump($objAnular);*/
 
  ?>
